c51f507701377a1233d36fdb4c3ef8bdd23e6d4a
willdavidc <willdavidc@gmail.com>
2016-06-27 02:20:52 -0400

    Moved to port 80
    Moved to port 80

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var port = 3000;
+var port = 80;
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
 http.listen(port, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:80');
 });
 
