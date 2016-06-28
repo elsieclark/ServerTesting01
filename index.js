229885d0eb9be4119a9161de08214a3ebcbdf6b7
willdavidc <willdavidc@gmail.com>
2016-06-28 10:29:16 -0400

    Changed to port 80
    Changed to port 80

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = 3000;
+var port = 80;
 
 var sequenceFromPi = ""
 var sequenceFromPhone = "[8, 8, 8, 8]"
@@ -44,4 +44,5 @@ http.listen(port, function(){
 setInterval(() => io.emit('sequencePi', sequenceFromPhone), 500);
 setInterval(() => io.emit('sequencePhone', sequenceFromPi), 500);
 setInterval(() => io.emit('messagePhone', Date.now()), 500);
+setInterval(() => console.log("Working...");
 
