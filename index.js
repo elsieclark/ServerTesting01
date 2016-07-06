6fc01599e33b62b540618a280e363ff5a301ba46
willdavidc <willdavidc@gmail.com>
2016-07-06 17:41:50 -0400

    Added initial portfolio template
    Added initial portfolio template html and css

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var port = 80;
 var sequenceFromPi = ""
 var sequenceFromPhone = "[8, 8, 8, 8]"
 
+var toLatinQueue = []
+
+var fromLatinQueue = []
+var toMorseQueue = []
+
 app.get('/', function(req, res){
   res.sendfile('index.html');
 });
@@ -17,7 +22,6 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-    
     
   socket.on('sequencePi', function(msg){
       if (sequenceFromPi != msg && msg != "") {
@@ -33,15 +37,86 @@ io.on('connection', function(socket){
       }
   });
     
+    
+  socket.on('messagePi', function(msg){
+      if (msg != "") {
+          fromMorseQueue.push([msg, Date.now()])
+      }
+  });
+    
+  socket.on('messagePhone', function(msg){
+      if (msg != "") {
+          fromLatinQueue.push([msg, Date.now()])
+      }
+  });
+    
 });
 
 http.listen(port, function(){
   console.log('listening on *:3000');
 });
 
+var translateToLatin = function(msg){
+    
+    var wordArr = msg.split(" ")
+    
+}
+
+var splitIntoLetters
+
 // http://[2620:101:f000:700:c9b8:4484:a65d:df7f]:3000/
 
 setInterval(() => io.emit('sequencePi', sequenceFromPhone), 500);
 setInterval(() => io.emit('sequencePhone', sequenceFromPi), 500);
 setInterval(() => io.emit('messagePhone', Date.now()), 500);
+
+var fromMorse = {
+    '.-':"a",
+    '-...':"b",
+    '-.-.':"c",
+    '-..':"d",
+    '.':"e",
+    '..-.':"f",
+    '--.':"g",
+    '....':"h",
+    '..':"i",
+    '.---':"j",
+    '-.-':"k",
+    '.-..':"l",
+    '--':"m",
+    '-.':"n",
+    '---':"o",
+    '.--.':"p",
+    '--.-':"q",
+    '.-.':"r",
+    '...':"s",
+    '-':"t",
+    '..-':"u",
+    '...-':"v",
+    '.--':"w",
+    '-..-':"x",
+    '-.--':"y",    
+    '--..':"z",  
+    '-----':"0",  
+    '.----':"1",  
+    '..---':"2",  
+    '...--':"3",  
+    '....-':"4",  
+    '.....':"5",  
+    '-....':"6",  
+    '--...':"7",  
+    '---..':"8",  
+    '----.':"9",  
+    '.-.-.-':".",  
+    '--..--':",",  
+    '---...':":",  
+    '..--..':"?",  
+    '.----.':"\'",  
+    '-....-':"-",  
+    '-..-.':"/",  
+    '.-..-.':"\"",  
+    '.--.-.':"@",  
+    '-...-':"="
+}
+
 
